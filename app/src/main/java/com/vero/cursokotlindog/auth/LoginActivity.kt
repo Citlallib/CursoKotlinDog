34536d24875d97e05fdd736b0673aa157d65549a
CitlalliB <citlalli.barragan@elektra.com.mx>
2022-09-14 21:31:48 -0500

    Agregar vistas para un Ãlogin y resgisto, navigation entre fragmentos

---
+++ app/src/main/java/com/vero/cursokotlindog/auth/LoginActivity.kt
@@ -0,0 +1,19 @@
+package com.vero.cursokotlindog.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import com.vero.cursokotlindog.R
+import com.vero.cursokotlindog.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity(), LoginFragment.LoginFragmentActions {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onRegisterButtonClick() {
+        findNavController(R.id.nav_host_fragment).navigate(LoginFragmentDirections.actionLoginFragmentToSignUpFragment())
+    }
+}
